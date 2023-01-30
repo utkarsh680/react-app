38d8165f133c975ea93106b4b720df66abed4ca5
utkarsh <singhrishu680@gmail.com>
2023-01-30 22:38:25 +0530

    add constructor and use

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react'
 
 export default class CartItem extends Component {
+    constructor (){
+        super();
+        this.state ={
+            price:999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+    }
     render() {
+        const {price , title, qty} = this.state;
         return (
             <div className='cart-item'>
                 <div className='left-block'>
@@ -11,12 +21,14 @@ export default class CartItem extends Component {
                 </div>
                 <div className='right-block'>
 
-                    <div style={{fontSize : 25}}>Phone</div>
-                    <div style={{color : '#777'}}>Rs 999</div>
-                    <div style={{color :'#777'}}>Qty: 1</div>
-
+                    <div style={{fontSize : 25}}>{title}</div>
+                    <div style={{color : '#777'}}>Rs {price}</div>
+                    <div style={{color :'#777'}}>Qty: {qty}</div>
                     <div className='cart-item-actions'>
                         {/* button */}
+                        <img alt = "increase"  className='action-icons' src='https://cdn-icons-png.flaticon.com/512/992/992651.png' />
+                        <img alt = "decerase"  className='action-icons' src='https://cdn-icons-png.flaticon.com/512/992/992683.png' />
+                        <img alt = "delete"  className='action-icons' src='https://cdn-icons-png.flaticon.com/512/1214/1214428.png' />
                     </div>
 
 
@@ -31,6 +43,7 @@ const styles ={
         height:110,
         width :110,
         borderRadius :4,
-        background:'#ccc'
+        background:'#ccc',
+        backgroundImage: "url('https://img.freepik.com/free-photo/top-view-indian-food-assortment_23-2148747714.jpg?w=1380&t=st=1675095396~exp=1675095996~hmac=bea9ba5cb3d48299f4eca03434758ec9b6a70b2fb8b9681542bcc60439a0ee1a')"
     }
 }
