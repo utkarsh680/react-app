e659db435d6c6272ef642f4280065f5bd2a9de3c
utkarsh <singhrishu680@gmail.com>
2023-02-05 23:58:59 +0530

    add cartCount function

--- src/Cart.js
+++ src/Cart.js
@@ -2,73 +2,10 @@ import React, { Component } from 'react';
 import CartItem from "./CartItem";
 
 
-export class Cart extends Component {
-    constructor() {
-        super();
-        this.state = {
-            products: [
-                {
-                    price: 99,
-                    title: 'Watch',
-                    qty: 1,
-                    img: '',
-                    id: 1
-                },
-                {
-                    price: 21999,
-                    title: 'Phone',
-                    qty: 1,
-                    img: '',
-                    id: 2
-                },
-                {
-                    price: 999,
-                    title: 'gun',
-                    qty: 1,
-                    img: '',
-                    id: 3
-                }
-            ]
-
-        }
-    }
-    handleIncreaseQuantity =(product) =>{
-        const {products} = this.state;
-        const index = products.indexOf(product);
-
-        products[index].qty += 1;
-
-        this.setState({
-            products:products
-        })
-    }
-
-    handleDecreaseQuantity =(product) =>{
-        const {products} = this.state;
-        const index = products.indexOf(product);
-        if(products[index].qty === 0 ){
-            return;
-        }
-
-        products[index].qty -= 1;
-
-        this.setState({
-            products:products
-        })
-    }
-    handleDeleteProduct = (id) => {
-        const {products} = this.state;
-
-        const items  =products.filter((items) => items.id !== id);
-
-        this.setState ({
-            products: items
-        }
-            
-        )
-    }
-    render() {
-        const { products } = this.state;
+const Cart = (props) => {
+    
+    
+        const { products } = props;
         return (
             <div className='cart'>
                 {products.map((product) => {
@@ -76,15 +13,15 @@ export class Cart extends Component {
                         <CartItem
                             product={product}
                             key={product.id} 
-                            onIncreaseQuantity = {this.handleIncreaseQuantity}
-                            onDecreaseQuantity ={this.handleDecreaseQuantity}
-                            onDeleteProduct ={this.handleDeleteProduct}
+                            onIncreaseQuantity = {props.onIncreaseQuantity}
+                            onDecreaseQuantity ={props.onDecreaseQuantity}
+                            onDeleteProduct ={props.onDeleteProduct}
                             />
                     )
                 })}
             </div>
         )
     }
-}
+
 
 export default Cart;
