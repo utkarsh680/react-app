033c96d3b6a31c93744e21a424522f7a4b3e4570
utkarsh <singhrishu680@gmail.com>
2023-02-09 12:53:14 +0530

    increase quantity in firebase

--- src/App.js
+++ src/App.js
@@ -58,10 +58,22 @@ class App extends React.Component {
     const { products } = this.state;
     const index = products.indexOf(product);
 
-    products[index].qty += 1;
+    // products[index].qty += 1;
 
-    this.setState({
-      products: products
+    // this.setState({
+    //   products: products
+    // })
+
+    const docRef = this.db.collection('products').doc(products[index].id);
+    docRef
+    .update({
+      qty: products[index].qty +1
+    })
+    .then(() => {
+    console.log('Updated successfully')
+    })
+    .catch((error) =>{
+      console.log('error', error);
     })
   }
 
@@ -139,7 +151,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Navbar count={this.getCartCount()} />
-        <button onClick={this.addProduct} style ={{padding:20, fontSize:20}}>Add a button</button>
+        {/* <button onClick={this.addProduct} style ={{padding:20, fontSize:20}}>Add a button</button> */}
         <Cart
           products={products}
           onIncreaseQuantity={this.handleIncreaseQuantity}
